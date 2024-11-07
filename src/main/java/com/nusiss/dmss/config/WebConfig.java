156662ac217cf98300726d2aa00be7be92ab3af3
WHY <1620283589@qq.com>
2024-11-07 18:06:07 +0800

    add CORS config

---
+++ src/main/java/com/nusiss/dmss/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.nusiss.dmss.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by WHY on 2024/11/7.
+ * Functions: CORS configuration
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
